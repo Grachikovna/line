1580eb38423033634a16da2028c514845aab1204
origin <maryna.oganesyan@yandex.ru>
2024-04-12 22:10:10 +0400

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task 1
+        System.out.println("task 1");
+        String lastName = "Ivanov";
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+        System.out.println();
+        //task 2
+        System.out.println("task 2");
+        String f = lastName + " " + firstName + " " + middleName;
+        System.out.println(f.toUpperCase());
+        System.out.println();
+        //task 3
+        System.out.println("task 3");
+        String q = "Иван Семён Семёнович";
+        q = q.replace("ё", "е");
+        System.out.println(q);
     }
 }
